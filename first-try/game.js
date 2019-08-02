f6486a67e99d73248f6a370740c38f81480e4692
Jordan Abbott <abbottjord94@gmail.com>
2019-08-02 16:09:40 -0700

    Add files via upload
    first couple of files

---
+++ first-try/game.js
@@ -0,0 +1,30 @@
+var config = {
+	type: Phaser.AUTO,
+	width: 1024,
+	height: 768,
+	physics: {
+		default: 'arcade',
+		arcade: {
+			
+		}
+	},
+	scene: {
+		preload: preload,
+		create: create,
+		update: update
+	}
+};
+
+var game = new Phaser.Game(config);
+
+function preload() {
+	
+}
+
+function create() {
+
+}
+
+function update() {
+	
+}
